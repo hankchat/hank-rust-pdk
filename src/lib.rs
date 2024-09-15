d6c556aa614e29d42792ef83ebf7974403fc647a
Marc Addeo <marcaddeo@gmail.com>
2024-09-15 15:49:49 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+use extism_pdk::{host_fn, Prost};
+use hank_types::cron::CronJob;
+use hank_types::database::{PreparedStatement, Results};
+use hank_types::message::{Message, Reaction};
+use hank_types::{
+    CronInput, CronOutput, DbQueryInput, DbQueryOutput, ReactInput, ReactOutput, SendMessageInput,
+    SendMessageOutput,
+};
+
+#[host_fn]
+extern "ExtismHost" {
+    pub fn send_message(input: Prost<SendMessageInput>) -> Prost<SendMessageOutput>;
+    pub fn react(input: Prost<ReactInput>) -> Prost<ReactOutput>;
+    pub fn db_query(input: Prost<DbQueryInput>) -> Prost<DbQueryOutput>;
+    pub fn cron(input: Prost<CronInput>) -> Prost<CronOutput>;
+}
+
+pub struct Hank;
+
+impl Hank {
+    pub fn new() -> Self {
+        Self
+    }
+
+    pub fn send_message(message: Message) {
+        let input = SendMessageInput {
+            message: Some(message),
+        };
+
+        let _ = unsafe { send_message(Prost(input)) };
+    }
+
+    pub fn react(reaction: Reaction) {
+        let input = ReactInput {
+            reaction: Some(reaction),
+        };
+
+        let _ = unsafe { react(Prost(input)) };
+    }
+
+    pub fn db_query(statement: PreparedStatement) -> Results {
+        let input = DbQueryInput {
+            prepared_statement: Some(statement),
+        };
+
+        let output = unsafe { db_query(Prost(input)) };
+        let Prost(output) = output.unwrap();
+
+        output.results.unwrap()
+    }
+
+    pub fn cron(cronjob: CronJob) {
+        let input = CronInput {
+            cron_job: Some(cronjob),
+        };
+
+        let _ = unsafe { cron(Prost(input)) };
+    }
+}
